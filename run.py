ab55e98e0b8d32a69481c036182a2c0bd2aed57e
6Viper <leofmetz@gmail.com>
2023-01-18 15:20:54 +0000

    Adjusting validation to verify all values given are integers

--- run.py
+++ run.py
@@ -35,6 +35,7 @@ def validate_data(values):
     or if there aren't exactly 9 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 9:
             raise ValueError(
                 f"Exactly 9 values required, you provided {len(values)}"
