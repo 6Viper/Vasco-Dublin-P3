4f3f0b530b3993aac0b31e0bcdc244aee1610db8
6Viper <leofmetz@gmail.com>
2023-01-19 16:22:00 +0000

    Remove \ feature to bring over next line print command as heroku display incorrect

--- run.py
+++ run.py
@@ -22,9 +22,7 @@ def get_sales_data():
 
     """
     while True:
-        print("Please enter total sold units from last month following product\
-        order (Polo S/Polo M/Polo L/T-Shirt S/T-shirt M/T-shirt L/Jacket S/Jacket\
-        M/Jacket L).")
+        print("Please enter total sold units from last month following product order (Polo S/Polo M/Polo L/T-Shirt S/T-shirt M/T-shirt L/Jacket S/Jacket M/Jacket L).")
         print("Data should be nine numbers, separated by commas.")
         print("Examples: 10,5,40,50,100,88,2,15,120\n")
 
